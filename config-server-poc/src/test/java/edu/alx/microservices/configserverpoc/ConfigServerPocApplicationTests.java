512d0ae36154dfc243dd1993a361e8c187ce92e7
Alex Contreras <a.contrerasgranados@gmail.com>
2019-06-21 01:07:29 -0500

    Config Server PoC!

---
+++ config-server-poc/src/test/java/edu/alx/microservices/configserverpoc/ConfigServerPocApplicationTests.java
@@ -0,0 +1,16 @@
+package edu.alx.microservices.configserverpoc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ConfigServerPocApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
